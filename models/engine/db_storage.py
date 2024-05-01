428cfe0cf80a17826d620e784569e3b25f0a2a7b
john-otienoh <johnteclaire@gmail.com>
2024-05-01 17:39:59 +0300

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+"""
+from os import getenv
+from sqlalchemy import create_engine
+
+class DBStorage:
+    """
+    """
+    __engine = None
+    __session = None
+
+    def  __init__(self):
+        """
+        """
+        user = getenv('HBNB_MYSQL_USER')
+        pwd = getenv('HBNB_MYSQL_PWD')
+        host  = getenv('HBNB_MYSQL_HOST')
+        db = getenv('HBNB_MYSQL_DB')
+
+        db_url = "mysql+mysqldb://{}:{}@{}/{}".format(user, pwd, host, db)
+
+        self.__engine = create_engine(db_url, pool_pre_ping=True)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __table_name__  = 'states'
+    name = Column(String(128), nullable=False)
+    if  getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """"""
+            import models
+            from models.city import City
+            city_list = []
+            for i in models.storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(i)
+            return city_list
